60a49206bcd7afdb678aae1a21775cb05f8665cc
yinguobing <yinguobing@gmail.com>
2018-12-09 18:55:14 +0800

    use Promise for db operation

--- Course_Blockchain_Data/Practice_Private_Blockchain_Code_Session/levelSandbox.js
+++ Course_Blockchain_Data/Practice_Private_Blockchain_Code_Session/levelSandbox.js
@@ -7,31 +7,48 @@ const chainDB = './chaindata';
 const db = level(chainDB);
 
 // Add data to levelDB with key/value pair
-function addLevelDBData(key,value){
-  db.put(key, value, function(err) {
-    if (err) return console.log('Block ' + key + ' submission failed', err);
+function addLevelDBData(key, value) {
+  return new Promise(function (resolve, reject) {
+    db.put(key, value, function (err) {
+      if (err) {
+        console.log('Block ' + key + ' submission failed', err);
+        reject(err)
+      }
+    })
+    resolve(db)
   })
 }
 
 // Get data from levelDB with key
-function getLevelDBData(key){
-  db.get(key, function(err, value) {
-    if (err) return console.log('Not found!', err);
-    console.log('Value = ' + value);
+function getLevelDBData(key) {
+  return new Promise(function (resolve, reject) {
+    db.get(key, function (err, value) {
+      if (err) {
+        console.log('Not found!', err);
+        reject(err)
+      }
+      console.log('Value = ' + value);
+      resolve(value)
+    })
   })
 }
 
 // Add data to levelDB with value
 function addDataToLevelDB(value) {
-    let i = 0;
-    db.createReadStream().on('data', function(data) {
-          i++;
-        }).on('error', function(err) {
-            return console.log('Unable to read data stream!', err)
-        }).on('close', function() {
-          console.log('Block #' + i);
-          addLevelDBData(i, value);
-        });
+  let i = 0;
+  return new Promise(function (resolve, reject) {
+    db.createReadStream()
+      .on('data', function (data) {
+        i++;
+      }).on('error', function (err) {
+        console.log('Unable to read data stream!', err)
+        reject(err)
+      }).on('close', function () {
+        console.log('Block #' + i);
+        addLevelDBData(i, value);
+      });
+  })
+
 }
 
 /* ===== Testing ==============================================================|
@@ -46,7 +63,7 @@ function addDataToLevelDB(value) {
 |  ===========================================================================*/
 
 
-(function theLoop (i) {
+(function theLoop(i) {
   setTimeout(function () {
     addDataToLevelDB('Testing data');
     if (--i) theLoop(i);
