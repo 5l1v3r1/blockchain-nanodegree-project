041296580569a13c05dfb88c16865df0d2975560
yinguobing <yinguobing@gmail.com>
2018-12-09 20:02:26 +0800

    Configure LevelDB to persist dataset

--- Course_Blockchain_Data/Project_2_es6_starter_code/LevelSandbox.js
+++ Course_Blockchain_Data/Project_2_es6_starter_code/LevelSandbox.js
@@ -12,30 +12,51 @@ class LevelSandbox {
     }
 
     // Get data from levelDB with key (Promise)
-    getLevelDBData(key){
+    getLevelDBData(key) {
         let self = this;
-        return new Promise(function(resolve, reject) {
-            // Add your code here, remember un Promises you need to resolve() or reject()
-        });
+        return new Promise(function (resolve, reject) {
+            self.db.get(key, function (err, value) {
+                if (err) {
+                    console.log('Not found!', err);
+                    reject(err);
+                }
+                console.log('Value = ' + value);
+                resolve(value);
+            });
+        }
     }
 
     // Add data to levelDB with key and value (Promise)
     addLevelDBData(key, value) {
         let self = this;
-        return new Promise(function(resolve, reject) {
-            // Add your code here, remember un Promises you need to resolve() or reject() 
+        return new Promise(function (resolve, reject) {
+            self.db.put(key, value, function (err) {
+                if (err) {
+                    console.log('Block ' + key + ' submission failed', err);
+                    reject(err);
+                }
+            });
+            resolve(key);
         });
     }
 
     // Method that return the height
     getBlocksCount() {
         let self = this;
-        return new Promise(function(resolve, reject){
-            // Add your code here, remember un Promises you need to resolve() or reject()
+        let block_count = 0
+        return new Promise(function (resolve, reject) {
+            self.db.createReadStream()
+                .on('data', function (data) {
+                    block_count++;
+                })
+                .on('error', function (err) {
+                    reject(err)
+                })
+                .on('close', function () {
+                    resolve(block_count);
+                });
         });
     }
-        
-
 }
 
 module.exports.LevelSandbox = LevelSandbox;
